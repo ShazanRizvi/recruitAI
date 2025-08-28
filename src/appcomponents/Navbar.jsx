6b2411848458b9725ba7f8fb11cd20f47ffd8383
Shivangi Dubey <shivangidubey@Shivangis-MacBook-Air.local>
2025-08-28 08:48:48 +0530

    Navbar setup

--- src/appcomponents/Navbar.jsx
+++ src/appcomponents/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { NavLink } from "react-router-dom";
 
+
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 ">
